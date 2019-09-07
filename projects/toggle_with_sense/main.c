ec1f87fd4b299dd2af164ebab0a093032ffe3727
stellarshenson <konrad.jelen@gmail.com>
2019-09-08 01:02:52 +0200

    implemented stable sense task

--- projects/toggle_with_sense/main.c
+++ projects/toggle_with_sense/main.c
@@ -19,6 +19,7 @@
 #include <esp8266.h>
 #include <FreeRTOS.h>
 #include <task.h>
+#include <string.h> 
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
@@ -26,24 +27,51 @@
 #include <espressif/esp_system.h>
 
 #define GPIO_ESPRESSO_TOGGLE	2
+#define GPIO_LED		2
 #define GPIO_ESPRESSO_SENSE	4
-#define ESPRESSO_SENSE_DELAY	500
+#define ESPRESSO_SENSE_DELAY	1500
+
+//pairing password to display, this will be embedded in the AP homepage
+#define CUSTOM_SECTION "<p><b>Homekit Accessory ID:</b><br> %s </p>"
+#define INITIAL_ACCESSORY_PASSWORD "000-00-000"
 
 void espresso_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
 homekit_characteristic_t espresso_on = HOMEKIT_CHARACTERISTIC_(ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(espresso_on_callback));
 void espresso_sense_task();
+void espresso_toggle(bool on);
+void led_write(bool on);
+void accessory_identify_task(void *_args);
 
+
+/**
+ * espresso callback function to receive val
+ * */
+void espresso_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
+    espresso_toggle(on.bool_value);
+}
 
 /**
  * toggle the switch
  * */
 void espresso_toggle(bool on) {
     //gpio_write(GPIO_ESPRESSO_TOGGLE, on ? 0 : 1);
+    printf("espresso toggle %u\n", on);
     gpio_write(GPIO_ESPRESSO_TOGGLE, 1);
     vTaskDelay(300 / portTICK_PERIOD_MS);
     gpio_write(GPIO_ESPRESSO_TOGGLE, 0);
+
+    //set value
+    espresso_on.value.bool_value = on;
+    led_write(on);
 }
 
+/**
+ * LED status light
+ * */
+void led_write(bool on) {
+    printf("led state: %u\n", on );
+    gpio_write(GPIO_LED, on ? 0 : 1);
+}
 
 /**
  * initialises the espresso state sense and prepare the toggle
@@ -54,10 +82,12 @@ void espresso_init() {
     //enable ESPRESSO_TOGGLE pin for OUTPUT and ESPRESSO_SENSE for INPUT
     gpio_enable(GPIO_ESPRESSO_TOGGLE, GPIO_OUTPUT);
     gpio_enable(GPIO_ESPRESSO_SENSE, GPIO_INPUT);
-    //espresso_toggle(espresso_on.value.bool_value);
+    gpio_set_pullup(GPIO_ESPRESSO_TOGGLE, false, false);
+    gpio_set_pullup(GPIO_ESPRESSO_SENSE, false, false);
+    led_write(false);
     
     //enable sense task
-    xTaskCreate(espresso_sense_task, "Espresso Sense", 128, NULL, 2, NULL);
+    xTaskCreate(espresso_sense_task, "Espresso Sense", 512, NULL, 3, NULL);
 }
 
 
@@ -67,63 +97,68 @@ void espresso_init() {
 void espresso_sense_task() {
     //monitor the status of espresso power
     homekit_value_t on;
-    on.bool_value = false;
 
     //get the status and notify homekit
-    on.bool_value = gpio_read(GPIO_ESPRESSO_TOGGLE);
-    homekit_characteristic_notify(&espresso_on, on);
+    //on.bool_value = gpio_read(GPIO_ESPRESSO_TOGGLE);
+    //bool on = gpio_read(GPIO_ESPRESSO_TOGGLE);
+    //homekit_characteristic_notify(&espresso_on, on);
 
 
     //keep monitoring by going idle for a predefined time and then sense again.
     //when status of the espresso changed - notify homekit
     while(1) {
 	vTaskDelay(ESPRESSO_SENSE_DELAY / portTICK_PERIOD_MS);
-	on.bool_value = gpio_read(GPIO_ESPRESSO_TOGGLE);
+	//on.bool_value = gpio_read(GPIO_ESPRESSO_TOGGLE);
+	on.bool_value = (rand() % 10 == 0 ? 0 : 1);
+	//gpio_read(GPIO_ESPRESSO_TOGGLE);
+	//printf("espresso sense, status:  %d\n", on.bool_value);
+	printf("espresso sense: %d\n", on.bool_value);
 	if( on.bool_value != espresso_on.value.bool_value ) homekit_characteristic_notify(&espresso_on, on);
     }
 
+    vTaskDelete(NULL);
 }
 
-void espresso_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
-    espresso_toggle(espresso_on.value.bool_value);
+/**
+ * identify accessort
+ * */
+void accessory_identify(homekit_value_t _value) {
+    printf("Accessory identify\n");
+    xTaskCreate(accessory_identify_task, "Accessory identify", 128, NULL, 2, NULL);
 }
 
-void led_identify_task(void *_args) {
+/**
+ * task run to identify the accessory
+ * */
+void accessory_identify_task(void *_args) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            espresso_toggle(true);
+            led_write(true);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            espresso_toggle(false);
+             led_write(false);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
 
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 
-    espresso_toggle(espresso_on.value.bool_value);
-
     vTaskDelete(NULL);
-}
-
-void led_identify(homekit_value_t _value) {
-    printf("LED identify\n");
-    xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
 }
 
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+            HOMEKIT_CHARACTERISTIC(NAME, "Espresso Switch"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Stellars"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "001"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Momentary Switch"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.3"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
             NULL
         }),
         HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(NAME, "Espresso Switch"),
             &espresso_on,
             NULL
         }),
@@ -138,7 +173,7 @@ homekit_accessory_t *accessories[] = {
  * */
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "000-00-000"
+    .password = INITIAL_ACCESSORY_PASSWORD
 };
 
 void on_wifi_ready() {
@@ -146,24 +181,34 @@ void on_wifi_ready() {
 }
 
 /**
- * generates accessory ID from the chipID, turns 32 bit chipID into 4-byte chunks and gets modulo 9 from each
+ * generates accessory password from the chip_id, turns 32 bit chipID into 4-bit chunks and gets modulo 9 from each
+ * and generates the custom section to be displayed with the wifi_config AP server
  * */
-void accessory_id_init() {
+void accessory_password_init() {
     uint32_t chipid = sdk_system_get_chip_id();
     uint8_t accessory_id_digits[] = { chipid >> 0 & 0xf % 9,  chipid >> 4 & 0xf % 9, chipid >> 8 & 0xf % 9, 
     	chipid >> 12 & 0xf % 9, chipid >> 16 & 0xf % 9, chipid >> 20 & 0xf % 9, chipid >> 24 & 0xf % 9, chipid >> 28 & 0xf % 9 }; 
     config.password = (char*) malloc( 11 * sizeof(char));
+    char *buffer = (char*) malloc(strlen(config.password) + strlen(CUSTOM_SECTION)); 
+
+    //write accessory password 
     snprintf(config.password, 11, "%u%u%u-%u%u-%u%u%u", accessory_id_digits[0], accessory_id_digits[1], accessory_id_digits[2], 
     	                               accessory_id_digits[3], accessory_id_digits[4], accessory_id_digits[5], accessory_id_digits[6], accessory_id_digits[7]);
+
+    printf(">>> accessory password: %s\n", config.password);
+
+    //write custom section with the password
+    snprintf(buffer, strlen(config.password) + strlen(CUSTOM_SECTION) + 1, CUSTOM_SECTION, config.password);
     
-    //set accessory ID to the generated one. This is used in the wifi-config library
-    accessory_id_set(config.password);
+    printf(">>> custom section: %s\n", buffer);
+    //set custom section. This is used in the wifi-config library
+    custom_section_set(buffer);
 }
 
 
 void user_init(void) {
-    uart_set_baud(0, 115200);
-    accessory_id_init();
+    uart_set_baud(0, 74880);
+    accessory_password_init();
     wifi_config_init("stellars", NULL, on_wifi_ready);
     espresso_init();
 }
