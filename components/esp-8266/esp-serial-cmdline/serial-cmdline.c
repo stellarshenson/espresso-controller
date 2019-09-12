5c8cc3e6d6031f2addffee4ef2552a65ee480cd9
stellarshenson <konrad.jelen@gmail.com>
2019-09-12 16:25:13 +0200

    changed config to private context

--- components/esp-8266/esp-serial-cmdline/serial-cmdline.c
+++ components/esp-8266/esp-serial-cmdline/serial-cmdline.c
@@ -25,9 +25,9 @@
 typedef struct {
     char *buffer;
     void  (*callback)(char*);
-} serial_cmdline_config_t;
+} serial_cmdline_context_t;
 
-serial_cmdline_config_t  *serial_cmdline_config;
+serial_cmdline_context_t  *serial_cmdline_context;
 
 
 /**
@@ -41,20 +41,20 @@ void serial_cmdline_task() {
 
     while(1) {
 	vTaskDelay(CMD_DELAY / portTICK_PERIOD_MS); //delay between consecutive read attempts
-	memset(serial_cmdline_config->buffer, 0, CMD_BUFFER_SIZE); //reset bufer to zeros
+	memset(serial_cmdline_context->buffer, 0, CMD_BUFFER_SIZE); //reset bufer to zeros
 	i = 0;
 	c = 0;
 
 	//read buffer until serial line is flushed
-	while( (c = uart_getc_nowait(CMD_UART_NUM)) != '\xFF' && i < CMD_BUFFER_SIZE ) serial_cmdline_config->buffer[i++] = c;
+	while( (c = uart_getc_nowait(CMD_UART_NUM)) != '\xFF' && i < CMD_BUFFER_SIZE ) serial_cmdline_context->buffer[i++] = c;
 
 	//replace newline with terminator
-	if(serial_cmdline_config->buffer[0] > 0 && serial_cmdline_config->buffer[i-1] == '\n') serial_cmdline_config->buffer[i-1] = '\0'; 
+	if(serial_cmdline_context->buffer[0] > 0 && serial_cmdline_context->buffer[i-1] == '\n') serial_cmdline_context->buffer[i-1] = '\0'; 
     
 	//process commands
-	if(serial_cmdline_config->buffer[0] > 0) { 
-	    serial_cmdline_config->callback(serial_cmdline_config->buffer); //run command interpreter
-	    memset(serial_cmdline_config->buffer, 0, CMD_BUFFER_SIZE); //reset bufer to zeros
+	if(serial_cmdline_context->buffer[0] > 0) { 
+	    serial_cmdline_context->callback(serial_cmdline_context->buffer); //run command interpreter
+	    memset(serial_cmdline_context->buffer, 0, CMD_BUFFER_SIZE); //reset bufer to zeros
 	}
     }
 
@@ -69,9 +69,9 @@ void serial_cmdline_init( void (*callback)(char*) ){
     INFO("serial-cmdline: staring serial listener");    
 
     //set the callback function
-    serial_cmdline_config = malloc(sizeof(serial_cmdline_config_t));
-    serial_cmdline_config->buffer = calloc(CMD_BUFFER_SIZE, sizeof(char)); 
-    serial_cmdline_config->callback = callback;
+    serial_cmdline_context = malloc(sizeof(serial_cmdline_context_t));
+    serial_cmdline_context->buffer = calloc(CMD_BUFFER_SIZE, sizeof(char)); 
+    serial_cmdline_context->callback = callback;
 
     //listen for what is sent over the serial line
     xTaskCreate(serial_cmdline_task, "Command Listener", 768, NULL, 0, NULL);
