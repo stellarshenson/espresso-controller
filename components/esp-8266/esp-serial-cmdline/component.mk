78da607eab2bc1a7c1e36146107646a8353bd1d5
stellarshenson <konrad.jelen@gmail.com>
2019-09-13 21:22:31 +0200

    removed esp-serial-cmdline local files (moving to repository)

--- components/esp-8266/esp-serial-cmdline/component.mk
+++
@@ -1,6 +0,0 @@
-INC_DIRS += $(esp-serial-cmdline_ROOT)
-
-esp-serial-cmdline_INC_DIR = $(esp-serial-cmdline_ROOT)
-esp-serial-cmdline_SRC_DIR = $(esp-serial-cmdline_ROOT)
-
-$(eval $(call component_compile_rules,esp-serial-cmdline))
